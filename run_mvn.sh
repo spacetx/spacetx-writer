11e200d224dd36e81b3964ad09895f9b17486967
jmoore <josh@glencoesoftware.com>
2018-10-31 10:18:43 +0000

    Use merge build

--- run_mvn.sh
+++ run_mvn.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
+set -e
+set -u
+
 DIR=/tmp/bio-formats-6.0.0-SNAPSHOT
 mkdir $DIR
 trap "rm -rf $DIR" EXIT
 
 cd $DIR
-git clone -b east --depth 1 https://github.com/ome/bio-formats-build
+# Requires exclusion of S3
+git clone -b east_merge_trigger --depth 1 https://github.com/snoopycrimecop/bio-formats-build
 pushd bio-formats-build
 
 export LANG=en_US.UTF-8 
@@ -13,4 +17,4 @@ export LANG=en_US.UTF-8
 git submodule update --init
 mvn clean install -DskipSphinxTests
 cd bioformats
-ant clean jars tools test
+ant tools
